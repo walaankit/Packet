6a5fd32737060e5361af1e87b5c7fd86ef140a7a
Ankit Wala <walaankit921@gmail.com>
2023-05-12 20:42:14 +0530

    Added artifact info

--- packet/build.gradle.kts
+++ packet/build.gradle.kts
@@ -1,23 +1,42 @@
 
 plugins {
-    id ("java-library")
-    id ("org.jetbrains.kotlin.jvm")
+    id ("com.android.library")
+    id ("kotlin-android")
     id ("maven-publish")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+android {
+    compileSdk = 33
 
-afterEvaluate {
-    publishing {
-        publications {
+    defaultConfig {
 
+        minSdk = 21
+        targetSdk = 33
+        vectorDrawables.useSupportLibrary = true
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
         }
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
+    implementation ("androidx.core:core-ktx:1.10.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            release(MavenPublication) {
+                from components.release
+                groupId = "com.github.walaankit"
+                artifactId = "Packet"
+                version = "0.1.1"
+            }
+        }
+    }
 }
